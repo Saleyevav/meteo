eaa337d4f1cbb3d549ba0f5f4ec2461d692f4ead
Alexandr Saleyev <saleyevav@yandex.ru>
2022-09-05 14:14:16 +0300

    change navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,11 +12,18 @@
     <v-app-bar-title class="text-h4 d-none d-md-flex">METEO</v-app-bar-title>
 
     <v-spacer></v-spacer>
-    <v-btn @click="$router.push('/')"> На главную </v-btn>
-    <v-btn @click="$router.push('/users')" v-if="$store.state.isAdmin">
+    <v-btn @click="$router.push('/')" v-if="$store.state.isLogin">
+      На главную
+    </v-btn>
+    <v-btn
+      @click="$router.push('/users')"
+      v-if="$store.state.isAdmin && $store.state.isLogin"
+    >
       Пользователи
     </v-btn>
-    <v-btn class="ml-3" @click="logout"> Выйти </v-btn>
+    <v-btn class="ml-3" @click="logout" v-if="$store.state.isLogin">
+      Выйти
+    </v-btn>
   </v-app-bar>
 </template>
 <script>
