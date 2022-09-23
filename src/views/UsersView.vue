80d63e087dc7344b2a87501d6c6d5c1d007f3059
Alexandr Saleyev <saleyevav@yandex.ru>
2022-09-23 14:14:29 +0300

    remake UsersView

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,3 +1,28 @@
+<script>
+import UserCard from "@/components/UserCard.vue";
+import AddUserForm from "@/components/AddUserForm.vue";
+import { usersService } from "@/API/usersService";
+import { ref, onMounted } from "vue";
+
+export default {
+  components: {
+    UserCard,
+    AddUserForm,
+  },
+
+  setup() {
+    const users = ref({});
+
+    function getUsers() {
+      users.value = usersService.getUsers();
+    }
+
+    onMounted(getUsers);
+
+    return { users, getUsers };
+  },
+};
+</script>
 <template>
   <v-main>
     <v-row class="mt-4" dense>
@@ -14,32 +39,3 @@
     <add-user-form class="mt-4" @addUser="getUsers" />
   </v-main>
 </template>
-<script>
-import UserCard from "@/components/UserCard.vue";
-import AddUserForm from "@/components/AddUserForm.vue";
-import { usersService } from "@/API/usersService";
-
-export default {
-  components: {
-    UserCard,
-    AddUserForm,
-  },
-
-  data() {
-    return {
-      users: {},
-    };
-  },
-
-  methods: {
-    getUsers() {
-      this.users = usersService.getUsers();
-    },
-  },
-
-  mounted() {
-    this.getUsers();
-  },
-};
-</script>
-<style></style>
