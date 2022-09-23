be13a1eb3af55500d6f0d6b5da795ead1c7e4b80
Alexandr Saleyev <saleyevav@yandex.ru>
2022-09-23 09:12:46 +0300

    remake TimerPanel

--- src/components/TimerPanel copy.vue
+++ src/components/TimerPanel copy.vue
@@ -1,19 +1,3 @@
-<script>
-import { watch, ref } from "vue";
-export default {
-  name: "timer-panel",
-  setup(props, { emit }) {
-    const min = 0;
-    const max = 60;
-    const slider = ref(0);
-    watch(slider, () => {
-      emit("changeSlider", Math.round(slider.value));
-    });
-
-    return { min, max, slider };
-  },
-};
-</script>
 <template>
   <v-card class="mt-6 pa-6">
     <v-card-title class="pl-0"
@@ -30,3 +14,21 @@ export default {
     </v-slider>
   </v-card>
 </template>
+<script>
+export default {
+  name: "timer-panel",
+  data() {
+    return {
+      min: 0,
+      max: 60,
+      slider: 0,
+    };
+  },
+
+  watch: {
+    slider() {
+      this.$emit("changeSlider", Math.round(this.slider));
+    },
+  },
+};
+</script>
